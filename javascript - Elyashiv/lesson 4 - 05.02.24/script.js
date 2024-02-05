141eed7621a4d38c44ff8c2d68b835be5fc9b9b1
niveenbader <neveen.bader@gmail.com>
2024-02-05 21:12:44 +0200

    update

--- javascript - Elyashiv/lesson 4 - 05.02.24/script.js
+++ javascript - Elyashiv/lesson 4 - 05.02.24/script.js
@@ -12,3 +12,90 @@ function minus() {
     document.getElementById("count").style.fontsize = counter + "px";
 
 }
+
+function cityChange() {
+    const city = document.getElementById("city").value;
+    if (city == "502") {
+        document.getElementById("output1").innerHTML = "Awsome";
+    } else {
+        document.getElementById("output1").innerHTML = "EHHHH";
+    }
+}
+
+/* function login() {
+    const name = document.getElementById("name").value;
+    const password = document.getElementById("password").value;
+    if (name == "banana" && password == "a123") {
+        document.getElementById("output2").innerHTML = "correct";
+    } else {
+        document.getElementById("output2").innerHTML = "wrong";
+    }
+} */
+function login() {
+    const name = document.getElementById("name").value;
+    const password = document.getElementById("password").value;
+    const output = document.getElementById("output2");
+
+
+    if (name == "banana" && password == "a123") {
+        output.innerHTML = "correct";
+        output.style.color = "blue";
+    } else {
+        output.innerHTML = "wrong";
+        output.style.color = "red";
+    }
+}
+
+/* function number() {
+    const num = +document.getElementById("num").value;
+    const output = document.getElementById("output3");
+
+    if (num <= "40") {
+        output.innerHTML = "red";
+        output.style.color = "red";
+    } else if
+        (num >= "41" && num <= "60") {
+        output.innerHTML = "orange";
+        output.style.color = "orange";
+    } else if
+        (num >= "61" && num <= "80") {
+        output.innerHTML = "blue";
+        output.style.color = "blue";
+
+
+    } else if
+        (num >= "81" && num <= "100") {
+        output.innerHTML = "green";
+        output.style.color = "green";
+    } else {
+        output.innerHTML = "חריג";
+        output.style.color = "pink";
+    }
+} */
+
+function number() {
+
+    const elem = document.getElementById("num");
+    if (elem.value > 100) {
+        elem.value = 0;
+    } else if (elem.value < 0) {
+        elem.value = 100;
+    }
+
+
+    const num = +elem.value;
+
+    if (num <= 40) {
+        elem.style.backgroundColor = "red";
+    } else if (num <= 60) {
+        elem.style.backgroundColor = "orange";
+    } else if (num <= 80) {
+        elem.style.backgroundColor = "blue";
+    } else if (num <= 100) {
+        elem.style.backgroundColor = "green";
+    } else {
+        elem.style.backgroundColor = "yellow";
+    }
+
+
+}
