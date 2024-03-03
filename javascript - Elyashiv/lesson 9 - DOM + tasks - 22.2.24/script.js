ca8f4108b469bc82a4a2f95f04a2b8fbed9d3bde
niveenbader <neveen.bader@gmail.com>
2024-03-03 13:30:58 +0200

    tasks

--- javascript - Elyashiv/lesson 9 - DOM + tasks - 22.2.24/script.js
+++ javascript - Elyashiv/lesson 9 - DOM + tasks - 22.2.24/script.js
@@ -1,19 +1,39 @@
-const tasks = document.querySelector(".tasks");
+// פתרון עם לחיצה על כפתור
+
+
+/* const tasks = document.querySelector('.tasks');
 
 
 function add() {
+
     const input = document.querySelector('input');
 
     const li = document.createElement('li');
+
+    li.innerHTML = input.value;
+    tasks.appendChild(li);
+
+    input.value = '';
+
+} */
+
+const tasks = document.querySelector('.tasks');
+
+
+function add() {
+
+    const input = document.querySelector('input');
+
+    const li = document.createElement('li');
+
     li.innerHTML = input.value;
     tasks.appendChild(li);
 
     input.value = '';
 }
 
-function keyup(ev) {
-    if (ev.key === 'Enter') {
-        add();
-    }
 
+function keyup(ev) {
+    if (ev.key === 'Enter')
+        add();
 }
