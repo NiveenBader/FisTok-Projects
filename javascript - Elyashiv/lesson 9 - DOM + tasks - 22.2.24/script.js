bef50474aba1942c805043475dad6f2e96da6959
niveenbader <neveen.bader@gmail.com>
2024-03-06 16:25:55 +0200

    update

--- javascript - Elyashiv/lesson 9 - DOM + tasks - 22.2.24/script.js
+++ javascript - Elyashiv/lesson 9 - DOM + tasks - 22.2.24/script.js
@@ -17,7 +17,7 @@ function add() {
 
 } */
 
-const tasks = document.querySelector('.tasks');
+/* const tasks = document.querySelector('.tasks');
 
 
 function add() {
@@ -42,6 +42,9 @@ function add() {
 
     tasks.appendChild(li);
 
+    const btn = document.createElement("button");
+    btn.innerHTML = 'Remove';
+    li.appendChild(btn);
     input.value = '';
 }
 
@@ -49,4 +52,56 @@ function add() {
 function keyup(ev) {
     if (ev.key === 'Enter')
         add();
+} */
+
+
+
+const tasks = document.querySelector(".tasks");
+
+// פונקציה להוספת אלמנטים
+function add() {
+    const input = document.querySelector('input');
+
+    // אם אין תוכן בתיבת הקלט - הפונקציה נעצרת
+    if (!input.value) {
+        return;
+    }
+
+    // יצירת אלמנט חדש
+    const li = document.createElement('li');
+
+    // הוספת האלמנט לתור הרשימה
+    tasks.appendChild(li);
+
+    // השמת התוכן של המשימה לאלמנט החדש
+    const div = document.createElement('div');
+    div.innerHTML = input.value;
+
+    // מאפשרים למשתמש לערוך את התוכן של האלמנט
+    div.contentEditable = true;
+    li.appendChild(div);
+
+    // יצירת לחצן מחיקה
+    const btn = document.createElement('button');
+    btn.innerHTML = 'x';
+
+    // יצירת אירוע - כל לחיצה על הלחצן תפעיל את הפונקציה של המחיקה
+    btn.addEventListener('click', function () {
+        const isAllowed = confirm(`האם אתה בטוח כי ברצונך למחוק את ${div.innerHTML}?`);
+
+        if (isAllowed) {
+            li.remove();
+        }
+    });
+    li.appendChild(btn);
+
+    // איפוס תיב הקלט
+    input.value = '';
+}
+
+// פונקציה להוספת אלמנטים באמצעות לחיצה על Enter
+function keyup(ev) {
+    if (ev.key == 'Enter') {
+        add();
+    }
 }
