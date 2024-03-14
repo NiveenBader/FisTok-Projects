f0f4dbe0f1f99ffc4871c0553b4ce3dade853767
niveenbader <neveen.bader@gmail.com>
2024-03-14 17:55:01 +0200

    UPDATE

--- javascript - Elyashiv/lesson 12 - 4.3.24/script.js
+++ javascript - Elyashiv/lesson 12 - 4.3.24/script.js
@@ -2,7 +2,8 @@ const clock = document.getElementById("analog-clock");
 
 for (let i = 0; i < 60; i++) {
     const div = document.createElement("div");
-    div.innerHTML = '';
+    div.innerHTML = '|';
+
     div.innerHTML += `<h2>${i}</h2>`;
     div.style.rotate = `${i * 6}deg`;
     clock.appendChild(div);
