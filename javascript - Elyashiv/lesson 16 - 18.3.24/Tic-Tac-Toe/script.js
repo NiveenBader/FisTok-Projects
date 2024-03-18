315a70a54354dc6072cac09b9f9c48a5bfc97f5f
niveenbader <neveen.bader@gmail.com>
2024-03-18 19:31:58 +0200

    update

--- javascript - Elyashiv/lesson 16 - 18.3.24/Tic-Tac-Toe/script.js
+++ javascript - Elyashiv/lesson 16 - 18.3.24/Tic-Tac-Toe/script.js
@@ -1,9 +1,14 @@
 const divs = document.querySelectorAll("#board>div");
 let isX = true;
 
+// לולאה העוברת על כל המשבצות
 for (const div of divs) {
+    // הוספת פונקציה המופעלת בעת לחיצה על אחת המשבצות
     div.addEventListener("click", function (ev) {
+        // האלמנט שעליו לחץ השחקן
         const elem = ev.target;
+
+        // אם המשבצת מלאה, הפונקציה נעצרת
         if (elem.innerText) {
             return;
         }
@@ -14,17 +19,24 @@ for (const div of divs) {
             elem.innerText = "O";
         }
 
+        // שינוי תור
         isX = !isX;
-        showTurn()
+        // הפעלת הפונקציה של המחווה הויזואלית
+        showTurn();
     });
 }
 
+/**
+ * פונקציה הנותנת מחווה של איזה שחקן התור הנוכחי
+ */
 function showTurn() {
+    // קודם כל, הסרנו את הקלאס מהשחקן האחרון
     document.querySelector('.currentTurn').classList.remove('currentTurn');
 
+    // שם את הקלאס בהתאם לתור השחקן
     if (isX) {
-        document.querySelector('#players>div:first-child').classList.add('currentTurn');
+        document.querySelector("#players>div:first-child").classList.add('currentTurn');
     } else {
-        document.querySelector('#players>div:last-child').classList.add('currentTurn');
+        document.querySelector("#players>div:last-child").classList.add('currentTurn');
     }
 }
