67d3179c042a16b2f48097e71786c43ffc0ff210
niveenbader <neveen.bader@gmail.com>
2024-04-15 19:33:02 +0300

    lesson23

---
+++ javascript - Elyashiv/lesson 23 - 15.4.23/students/script.js
@@ -0,0 +1,50 @@
+class student {
+    //אובייקט של סטודנט
+    student;
+
+    getFullName() {
+        return `${this.student.firstName} ${this.student.lastName}`;
+    }
+
+
+    getAge() {
+        const currentYear = new Date().getFullYear();
+        const studentYear = new Date(this.student.birthday).getFullYear();
+        return currentYear - studentYear;
+    }
+    getAverageTest() {
+        const sum = this.student.grades.reduce((res, num) => res += num, 0);
+        return sum / this.grades.length;
+    }
+    getTestAmount() {
+        return this.student.grades.length;
+    }
+
+
+    constructor(firstName, lastName, phone, birthday, city, grades) {
+        this.student = student;
+    }
+
+}
+
+const student1 = new student("Neveen", "Bader", "0505050505", "1994-05-25", "Yad-Hashmona", [100, 100, , 98, 99]);
+
+(async function getstudents() {
+    const res = await fetch("../students.json");
+    const data = await res.json();
+    const students = data.map(s => new students(s));
+})
+
+// יצירת ציונים פיקטיביים לכל סטודנט
+/* students.forEach(s => {
+    s.grades = [];
+
+    // טווח של 3 עד 8
+    const max = Math.floor(Math.random() * 6) + 3;
+
+    for (let i = 0; i < max; i++) {
+        // טווח של 40 עד 100
+        const g = Math.floor(Math.random() * 61) + 40;
+        s.grades.push(g);
+    }
+}); */
