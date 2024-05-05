ba5d20a45b8bdc01e2035cd09c89d0dc1386bb4c
niveenbader <neveen.bader@gmail.com>
2024-05-05 06:02:26 +0300

    update

--- javascript - Elyashiv/lesson 24 - OOP 18.4.24/script.js
+++ javascript - Elyashiv/lesson 24 - OOP 18.4.24/script.js
@@ -1,12 +1,34 @@
 class Gallery {
+    imager = [];
+    imgElem = document.createElement("img");
+    currentImage = 0;
 
 
-    constructor(elemId, ...images) {
+    constructor(elemId, ...imagesArr) {
         const galleryElem = document.getElementById(elemId);
-        console.log(galleryElem, images);
+        this.images = imagesArr;
+
+        galleryElem.appendChild(this.imgElem);
+        this.showImage();
+        this.imgElem.addEventListener("click", () => this.nextImage());
+
+    }
+
+    nextImage() {
+        this.currentImage++;
+
+        if (this.currentImage >= this.images.length) {
+            this.currentImage = 0;
+        }
+        this.showImage();
+    }
+
+    showImage() {
+        this.imgElem.src = `./images/${this.images[this.currentImage]}`;
     }
 }
 
-const gallery1 = new Gallery("gallery1", "(1).jpg", "(2).jpg", "(3).jpg", "(4).jpg");
-const gallery2 = new Gallery("gallery1", "(5).jpg", "(6).jpg", "(7).jpg", "(8).jpg");
-const gallery3 = new Gallery("gallery1", "(9).jpg", "(10).jpg", "(11).jpg", "(12).jpg"); 
+new Gallery("gallery", "(1).jpg", "(2).jpg", "(3).jpg", "(4).jpg");
+new Gallery("gallery", "(5).jpg", "(6).jpg", "(7).jpg", "(8).jpg");
+new Gallery("gallery", "(9).jpg", "(10).jpg", "(11).jpg", "(12).jpg");
+new Gallery("gallery", "(13).jpg", "(14).jpg", "(15).jpg", "(16).jpg");
